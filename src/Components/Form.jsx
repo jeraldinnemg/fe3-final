6d2278ae8846e067f4ed2fa0b03baddae54c783a
Jeraldinne Molleda <jeraldinnemg@gmail.com>
2023-04-05 12:53:38 -0300

    form-validaciones

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,15 +1,81 @@
-import React from "react";
-
+import React, { useState } from "react";
 
 const Form = () => {
-  //Aqui deberan implementar el form completo con sus validaciones
+  const [values, setValues] = useState({
+    nombre: "",
+    email: "",
+  });
+  const [errors, setErrors] = useState({});
+  const [submitted, setSubmitted] = useState(false);
+
+  const enviarForm = (e) => {
+    e.preventDefault();
+    const validationErrors = validarCampos(values);
+    setErrors(validationErrors);
+    if (Object.keys(validationErrors).length === 0) {
+      console.log(values);
+      setSubmitted(true);
+      document.querySelector(
+        ".submitted-message"
+      ).innerHTML = `<h3>Gracias ${values.nombre}, te contactaremos cuanto antes vía email</h3>`;
+    }
+  };
+
+  //Validaciones
+  const validarCampos = (values) => {
+    const errors = {};
+    if (!values.nombre) {
+      errors.nombre = "Debe ingresar un nombre válido";
+    } else if (values.nombre.length < 5) {
+      errors.nombre = "El nombre debe tener más de 5 letras";
+    }
+    if (!values.email) {
+      errors.email = "Debe ingresar un email válido";
+    } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+      errors.email = "Debe ingresar un email válido";
+    }
+    return errors;
+  };
+
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
 
   return (
-    <div>
-      <form>
+    <div className="form-container">
+      <h2>Formulario de contacto</h2>
+      <form className="form" onSubmit={enviarForm}>
+        <div className="form-group">
+          <label htmlFor="nombre">Nombre completo</label>
+          <input
+            type="text"
+            id="nombre"
+            name="nombre"
+            value={values.nombre}
+            onChange={handleChange}
+          />
+          {errors.nombre && <p className="error">{errors.nombre}</p>}
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+          />
+          {errors.email && <p className="error">{errors.email}</p>}
+        </div>
+        <button type="submit">Enviar</button>
       </form>
+      <div className="submitted-message"></div>
     </div>
   );
 };
 
 export default Form;
+
+
+
+
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from '../Components/Card'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Home = () => {
+  const [dentist,setDentists] = useState([])
+
+  const getDentists = async() => {
+    const data = await fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => {
+      console.log("Respuesta", response)
+      return response.json()
+    });
+    setDentists(data)
+  }
+
+  useEffect(()=> {
+    getDentists()
+  }, [])
   return (
     <main className="" >
       <h1>Home</h1>
       <div className='card-grid'>
         {/* Aqui deberias renderizar las cards */}
+        {dentist.map((dentist) => {
+          return (<Card key = {dentist.id} name = {dentist.name} id={dentist.id} username ={dentist.username}/>)
+        })}
       </div>
     </main>
   )
