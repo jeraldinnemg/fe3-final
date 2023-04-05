036e13612ae6d7158c63175138e1fd571c544082
Jeraldinne Molleda <jeraldinnemg@gmail.com>
2023-04-05 13:28:24 -0300

    details

--- src/Routes/Contact.jsx
+++ src/Routes/Contact.jsx
@@ -1,16 +1,76 @@
-import React from 'react'
-import Form from '../Components/Form'
+import React, { useState } from "react";
 
-//Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
+const Form = () => {
+  const [values, setValues] = useState({
+    nombre: "",
+    email: "",
+  });
+  const [errors, setErrors] = useState({});
+  const [submitted, setSubmitted] = useState(false);
 
-const Contact = () => {
+  const enviarForm = (e) => {
+    e.preventDefault();
+    const validationErrors = validarCampos(values);
+    setErrors(validationErrors);
+    if (Object.keys(validationErrors).length === 0) {
+      console.log(values);
+      setSubmitted(true);
+      document.querySelector(
+        ".submitted-message"
+      ).innerHTML = `<h3>Gracias ${values.nombre}, te contactaremos cuanto antes vía email</h3>`;
+    }
+  };
+
+  const validarCampos = (values) => {
+    const errors = {};
+    if (!values.nombre) {
+      errors.nombre = "Debe ingresar un nombre válido";
+    } else if (values.nombre.length < 5) {
+      errors.nombre = "El nombre debe tener más de 5 letras";
+    }
+    if (!values.email) {
+      errors.email = "Debe ingresar un email válido";
+    } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+      errors.email = "Debe ingresar un email válido";
+    }
+    return errors;
+  };
+
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
   return (
-    <div>
-      <h2>Want to know more?</h2>
-      <p>Send us your questions and we will contact you</p>
-      <Form/>
+    <div className="form-container">
+      <h2>Formulario de contacto</h2>
+      <form className="form" onSubmit={enviarForm}>
+        <div className="form-group">
+          <label htmlFor="nombre">Nombre completo</label>
+          <input
+            type="text"
+            id="nombre"
+            name="nombre"
+            value={values.nombre}
+            onChange={handleChange}
+          />
+          {errors.nombre && <p className="error">{errors.nombre}</p>}
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+          />
+          {errors.email && <p className="error">{errors.email}</p>}
+        </div>
+        <button type="submit">Enviar</button>
+      </form>
+      <div className="submitted-message"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Form;
--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -1,17 +1,54 @@
-import React from 'react'
-
+import React, {useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Detail = () => {
  
   // Consumiendo el parametro dinamico de la URL deberan hacer un fetch a un user en especifico
+  const [odontologo, setOdontologo] = useState(null);
+  const { id } = useParams();
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/users/${id}`
+        );
+        const data = await response.json();
+        setOdontologo(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, [id]);
+
+  console.log(odontologo);
   return (
     <>
       <h1>Detail Dentist id </h1>
       {/* aqui deberan renderizar la informacion en detalle de un user en especifico */}
       {/* Deberan mostrar el name - email - phone - website por cada user en especifico */}
+      <table>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Email</th>
+            <th>Teléfono</th>
+            <th>Sitio web</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{odontologo?.name}</td>
+            <td>{odontologo?.email}</td>
+            <td>{odontologo?.phone}</td>
+            <td>{odontologo?.website}</td>
+          </tr>
+        </tbody>
+      </table>
     </>
   )
 }
