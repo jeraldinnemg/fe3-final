0b19ebe04b56686db2cabd518d311440d4df228f
Jeraldinne Molleda <jeraldinnemg@gmail.com>
2023-04-06 13:13:40 -0300

    styles

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-
+import React, {useContext} from 'react'
+import { ContextGlobal } from "../Components/utils/global.context";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const Footer = () => {
+
+  useContext(ContextGlobal)
+  
   return (
     <footer>
+            <button className='back-to-top' onClick={() => {window.scrollTo({top: 0, behavior: "smooth"})}}>VOLVER ARRIBA</button>
+        <div className='footer-container'>
         <p>Powered by</p>
-        <img src="./images/DH.png" alt='DH-logo' />
+          <div className='footer-img'>
+            <img src="/images/DH.png"  alt='DH-logo'/>
+          </div>
+          <div className='social-media'>
+            <a href="https://www.facebook.com" rel="noreferrer" target="_blank"><i className="fa-brands fa-facebook"></i></a>
+            <a href="https://www.instagram.com" rel="noreferrer" target="_blank"><i className="fa-brands fa-instagram"></i></a>
+            <a href="https://www.tiktok.com" rel="noreferrer" target="_blank"><i className="fa-brands fa-tiktok"></i></a>
+            <a href="https://www.whatsapp.com" rel="noreferrer" target="_blank"><i className="fa-brands fa-whatsapp"></i></a>
+          </div>
+        </div>
+
+        
     </footer>
   )
 }
